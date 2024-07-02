d37f0e8b804954dcf40d2cd72d860aa817914727
ying1488 <yingecteach@gmail.com>
2024-07-02 21:14:12 +1000

    Registering

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const  mongoose  = require('mongoose');
+const bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb://localhost:27017/authDemo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 app.set('view engine','ejs');
 app.set('views','views');
 
+app.use(express.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+  res.send('This is the home page')
+})
 app.get('/register',(req,res)=>{
   res.render('register')
+})
+
+app.post('/register',async(req,res)=>{
+const {password,username} = req.body;
+  const hash = await bcrypt.hash(password,12);
+  const user = new User({
+    username, 
+    password: hash
+  })
+  await user.save();
+  res.redirect('/')
 })
 
 app.get('/secret',(req,res)=>{
